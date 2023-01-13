aefaa527d735330bf51b81bf06671518f85d8b4d
admin <dextercch.9033@gmail.com>
2023-01-13 13:42:48 +0800

    Initial Commit

---
+++ MusicRecognitionApp02/MusicRecognitionApp02App.swift
@@ -0,0 +1,17 @@
+//
+//  MusicRecognitionApp02App.swift
+//  MusicRecognitionApp02
+//
+//  Created by Tim on 13/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicRecognitionApp02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
